2dbe5d723fe00c8f045595411e0708b87867c06b
afaqahmad2005 <afaqahmadshewa1234@gmail.com>
2026-02-03 12:52:47 +0500

    Update

--- variable.py
+++ variable.py
@@ -23,3 +23,13 @@ intro = "Hello! How are you?"
 
 # Create a variable is_good
 is_good = True
+
+savings = 100
+new_savings = 40
+
+# Calculate total_savings using savings and new_savings
+total_savings = savings +new_savings
+print(total_savings)
+
+# Print the type of total_savings
+print(type(total_savings))
